81459303c7bebbe3c54bf6ccc3b6a8b06b560fe0
Mysha2009 <shamygounon@gmail.com>
2024-09-07 18:26:22 +0100

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,19 +18,31 @@ function App() {
      {
      
      <div className='App'>
-      <h1>
-        Sur les épaules de mon talent
-      </h1>
+        <h1>
+          WeCareer
+        </h1><br />
+        <h3>Votre carriere commence ici✨<br />
+          Remplissez ce formulaire👇🏿 pour commencer votre parcours</h3><br />
+        <form class="max-w-sm mx-auto">
+          <div class="mb-5">
+            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Niveau </label>
+            <input type="text" id="level" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="bac+3" required />
+          </div>
+          
+          <div class="mb-5">
+            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Specialité </label>
+            <input type="text" id="level" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Genie logiciel" required />
+          </div>
+          
+          <div class="mb-5">
+            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Metier envisagé </label>
+            <input type="text" id="level" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Architecture" required />
+          </div>
+          
+          
+          <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
 
-      <form action="/search">
-      <input type="text" />
-      </form>
-
-      <button type='submit'>
-        Submit it
-      </button>
-
-      <h1 className="text-center text-3xl font-bold underline">Hello World</h1>
 
      </div>
 
